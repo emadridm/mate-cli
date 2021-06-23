59d2de4c530ea4e9f5f42732b68b673578f7f7ef
Enrique Madrid <emadridm@gmail.com>
2021-06-22 20:10:51 -0500

    ./lib goodbye

--- lib/core/app.d.ts
+++
@@ -1,9 +0,0 @@
-import { Settings } from './settings';
-export declare class App {
-    static Name: string;
-    static Home: string;
-    static DefaultHome: string;
-    static DefaultSettingsFilepath: string;
-    static DefaultSettings: Settings;
-    private constructor();
-}
--- lib/core/settings.d.ts
+++
@@ -1,5 +0,0 @@
-export declare type Settings = {
-    [key: string]: any;
-};
-export declare function LoadSettings<T extends Settings>(filepath: string, defaultSettings: T): Promise<T>;
-export declare function SaveSettings<T extends Settings>(settings: T, filepath: string): Promise<void>;
